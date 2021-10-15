cfb60225a7b4bca35db413de1d700f5ffa13cfa0
Xamarin how-to <54463568+xamarinhowto@users.noreply.github.com>
2021-10-15 12:50:43 +1000

    Initial Commit

---
+++ XhtCollectionViewAlternatingRowColor/Converters/IndexToColorConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using Xamarin.Forms;
+
+namespace XhtCollectionViewAlternatingRowColor.Converters
+{
+    public class IndexToColorConverter : IValueConverter
+    {
+        public Color EvenColor { get; set; }
+
+        public Color OddColor { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var collectionView = parameter as CollectionView;
+
+            return collectionView.ItemsSource.Cast<object>().ToList().IndexOf(value) % 2 == 0 ? EvenColor : OddColor;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ XhtCollectionViewAlternatingRowColor/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace XhtCollectionViewAlternatingRowColor
+{
+    public class MainViewModel
+    {
+        public List<string> Items { get; set; }
+
+        public MainViewModel()
+        {
+            var items = new List<string>();
+
+            for (int i = 0; i < 20; i++)
+            {
+                items.Add(i.ToString());
+            }
+
+            Items = new List<string>(items);
+        }
+    }
+}
